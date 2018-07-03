712238ead2c58afa96e982c2df151f55f7f74c29
annakutti <sanyasonys@gmail.com>
2018-07-03 20:13:21 +0530

    updated

--- src/main/java/com/sany/helloboot/hellobootapp/controller/HelloController.java
+++ src/main/java/com/sany/helloboot/hellobootapp/controller/HelloController.java
@@ -14,6 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {	
 	@GetMapping("/hello")
 	public String getHello(){
-		return "say hello";
+		return "say hello to u";
 	}
 }
