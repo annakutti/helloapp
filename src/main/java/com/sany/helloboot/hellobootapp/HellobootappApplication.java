03b3af6ce2f3274a1b2a3cb4d450838f5b8ecd1e
annakutti <sanyasonys@gmail.com>
2018-07-03 20:05:25 +0530

    say hello

---
+++ src/main/java/com/sany/helloboot/hellobootapp/HellobootappApplication.java
@@ -0,0 +1,12 @@
+package com.sany.helloboot.hellobootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellobootappApplication.class, args);
+	}
+}
---
+++ src/main/java/com/sany/helloboot/hellobootapp/controller/HelloController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.sany.helloboot.hellobootapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Sanya_s
+ *
+ */
+@RestController
+public class HelloController {	
+	@GetMapping("/hello")
+	public String getHello(){
+		return "say hello";
+	}
+}
